3ad64c941bea1eb7472f09bc8a50ac7115be3639
newbe36524 <newbe36524@qq.com>
2023-08-11 23:35:43 +0800

    init

---
+++ api/TurboHub.cs
@@ -0,0 +1,53 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace RushGet.Function
+{
+    public class TurboHub
+    {
+        private readonly ILogger _logger;
+
+        public TurboHub(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<TurboHub>();
+        }
+
+        [Function("TurboHub")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            // get uri parameters from query string
+            // send get request to uri and return the response to the client
+            var uri = req.Query["uri"];
+            if (!string.IsNullOrWhiteSpace(uri))
+            {
+                _logger.LogInformation("uri: {Uri}", uri);
+                var httpClient = new HttpClient();
+                var response = httpClient.GetAsync(uri).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    _logger.LogInformation("Success to get response from uri");
+                    var rep = req.CreateResponse(HttpStatusCode.InternalServerError);
+                    rep.WriteString("Failed to get response from uri");
+                    return rep;
+                }
+                else
+                {
+                    _logger.LogInformation("Failed to get response from uri");
+                    var rep = req.CreateResponse(HttpStatusCode.OK);
+                    rep.Headers.Add("Content-Type", "application/octet-stream");
+                    response.Content.ReadAsStream().CopyTo(rep.Body);
+                    return rep;
+                }
+            }
+            else
+            {
+                _logger.LogInformation("uri is null or empty");
+                var response = req.CreateResponse(HttpStatusCode.NotFound);
+                response.WriteString("Please pass a name on the query string");
+                return response;
+            }
+        }
+    }
+}
