931bc1a4e486ae9d15a812c1ec2ebdba4ae03f0e
newbe36524 <newbe36524@qq.com>
2023-08-12 11:52:15 +0800

    support release and source code

--- api/TurboHub.cs
+++ api/TurboHub.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Text.RegularExpressions;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
@@ -14,9 +15,27 @@ namespace RushGet.Function
             _logger = loggerFactory.CreateLogger<TurboHub>();
         }
 
+        /// <summary>
+        /// check uri should be release or zip code
+        /// template:
+        /// https://github.com/${org}/${project}/archive/refs/heads/${branch}.zip
+        /// https://github.com/${org}/${project}/releases/download/${tag}/${file}
+        /// </summary>
+        /// <returns></returns>
+        private static IEnumerable<Regex> UriCheckRegexes()
+        {
+            yield return new Regex(
+                @"^https://github.com/.+/.+/archive/refs/heads/.+.zip$",
+                RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.IgnorePatternWhitespace);
+            yield return new Regex(
+                @"^https://github.com/.+/.+/releases/download/.+/.+$",
+                RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.IgnorePatternWhitespace);
+        }
+
         [Function("TurboHub")]
         public async Task<HttpResponseData> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")]
+            HttpRequestData req)
         {
             // get uri parameters from query string
             // send get request to uri and return the response to the client
@@ -24,6 +43,24 @@ namespace RushGet.Function
             if (!string.IsNullOrWhiteSpace(uri))
             {
                 _logger.LogInformation("uri: {Uri}", uri);
+                bool matched = false;
+                foreach (var regex in UriCheckRegexes())
+                {
+                    if (regex.IsMatch(uri))
+                    {
+                        matched = true;
+                        _logger.LogInformation("uri matched rule: {Rule}", regex.ToString());
+                    }
+                }
+
+                if (!matched)
+                {
+                    _logger.LogInformation("uri not matched any rule");
+                    var rep = req.CreateResponse(HttpStatusCode.BadRequest);
+                    await rep.WriteStringAsync("uri not matched any rule");
+                    return rep;
+                }
+
                 var httpClient = new HttpClient();
                 var response = await httpClient.GetAsync(uri);
                 if (response.IsSuccessStatusCode)
